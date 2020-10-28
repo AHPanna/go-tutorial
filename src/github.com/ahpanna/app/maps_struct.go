e33cce9c513dcb439bb2cc3fd62e501838fb26d5
Panna ABDUL HAKIM <panna_abdul_hakim@carrefour.com>
2020-10-28 22:30:17 +0100

    add struct

--- src/github.com/ahpanna/app/maps_struct.go
+++ src/github.com/ahpanna/app/maps_struct.go
@@ -46,5 +46,60 @@ func main() {
 
 	// struct
 	// 	https: //youtu.be/YS4e4q9oBaU?t=8913
+	aDoctor := Doctor{ // you can leave the key mapping but it's recommended for readability
+		number:    69,
+		actorName: "Azures",
+		companions: []string{
+			"Mylene",
+			"Caroline",
+			"Ab",
+			"Mai",
+		},
+	}
+	fmt.Println(aDoctor)
+	fmt.Println(aDoctor.actorName)
+	fmt.Println(aDoctor.companions[0])
 
+	// volatile struct short live. -- not recommended
+	aShortLiveStruct := struct{ name string }{name: "Azures"}
+	fmt.Println(aShortLiveStruct)
+	anotherShortLiveStruct := aShortLiveStruct
+	anotherShortLiveStruct.name = "Mylene"
+	fmt.Println(anotherShortLiveStruct)
+
+	// advanced struct inheritance
+	wildBird := Bird{}
+	wildBird.Name = "Abid"
+	wildBird.Origin = "Mars"
+	wildBird.speed = 300.5
+	wildBird.canFly = true
+	fmt.Println(wildBird)
+	fmt.Println(wildBird.Name)
+	// reommended
+	aBird := Bird{
+		Animal: Animal{Name: "Nusrat", Origin: "Asia"},
+		speed:  24.2,
+		canFly: false,
+	}
+	fmt.Println(aBird)
+	fmt.Println(aBird.Name)
+}
+
+// struct can only be declared outside of a function
+type Doctor struct {
+	number     int
+	actorName  string
+	companions []string
+}
+
+// advanced struct inheritance
+type Animal struct {
+	Name   string
+	Origin string
+}
+type Bird struct {
+	// let's add instance of animal
+	Animal
+	speed  float32
+	canFly bool
 }
